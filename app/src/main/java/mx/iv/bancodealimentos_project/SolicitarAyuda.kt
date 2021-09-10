662a671a4614033554ddd2b6f22628ef65e6ac2d
IvannOz <ivannos324@gmail.com>
2021-09-10 15:25:46 -0500

    Intento de hacer transacciones en fragments

--- app/src/main/java/mx/iv/bancodealimentos_project/SolicitarAyuda.kt
+++ app/src/main/java/mx/iv/bancodealimentos_project/SolicitarAyuda.kt
@@ -6,16 +6,24 @@ import android.view.View
 import android.widget.CheckBox
 import android.widget.Toast
 import mx.iv.bancodealimentos_project.R
+import mx.iv.bancodealimentos_project.fragments.LaAyudaEs
 import mx.iv.bancodealimentos_project.fragments.MenuFragment
+import mx.iv.bancodealimentos_project.fragments.ParaMiFragment
 
-class SolicitarAyuda : AppCompatActivity(), MenuFragment.CallbackMenu {
+class SolicitarAyuda : AppCompatActivity(), MenuFragment.CallbackMenu, LaAyudaEs.CallbackHelp {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_solicitar_ayuda)
 
-        val menuFragment = MenuFragment();
+        val menuFragment = MenuFragment()
         val transaction = supportFragmentManager.beginTransaction()
         transaction.add(R.id.MenuSolicitarAyuda, menuFragment, TAG_FRAGMENT)
+        transaction.commit()
+
+        val ayudaEs = LaAyudaEs()
+        val transact = supportFragmentManager.beginTransaction()
+        transact.add(R.id.Options, ayudaEs, TAG_FRAGMENT)
+        transact.commit()
     }
 
     override fun returnAct() {
@@ -23,13 +31,17 @@ class SolicitarAyuda : AppCompatActivity(), MenuFragment.CallbackMenu {
     }
 
     fun onCheckboxClicked(view: View) {
-        if (view is CheckBox) {
+    /*    if (view is CheckBox) {
             val checked: Boolean = view.isChecked
 
             when (view.id) {
                 R.id.MeCheckBox -> {
                     if (checked) {
-                        Toast.makeText(this, "Me picado", Toast.LENGTH_SHORT).show()
+
+                        val paraMe = ParaMiFragment()
+                        val transaction = supportFragmentManager.beginTransaction()
+                        transaction.add(R.id.AyudaMe, paraMe, TAG_FRAGMENT)
+                        transaction.commit()
                     } else {
                         Toast.makeText(this, "Me despicado", Toast.LENGTH_SHORT).show()
                     }
@@ -43,10 +55,24 @@ class SolicitarAyuda : AppCompatActivity(), MenuFragment.CallbackMenu {
                 }
             }
         }
+
+     */
     }
 
 
     companion object {
         private const val TAG_FRAGMENT = "fragment"
+    }
+
+    override fun replaceParaMiFragment() {
+        var currentFragment = supportFragmentManager.findFragmentByTag(TAG_FRAGMENT)
+        var transaction = supportFragmentManager.beginTransaction()
+        if(currentFragment != null) {
+            transaction.remove(currentFragment)
+        }
+        val paraMi = ParaMiFragment()
+        transaction.add(R.id.Options, paraMi, TAG_FRAGMENT)
+        transaction.commit()
+
     }
 }
