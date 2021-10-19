2638ca71b02205f75dac7b429b747fc193cf5b96
Maribel Melendez <lizbethmelendez@live.com>
2021-10-18 23:35:11 -0500

    Comentar clases helper

--- app/src/main/java/mx/iv/bancodealimentos_project/helper/DonationsAdapter.kt
+++ app/src/main/java/mx/iv/bancodealimentos_project/helper/DonationsAdapter.kt
@@ -11,18 +11,22 @@ import mx.iv.bancodealimentos_project.R
 import java.util.ArrayList
 
 class DonationsAdapter(donationsList: ArrayList<DonationData>) : RecyclerView.Adapter<DonationsAdapter.DonationsAdapterViewHolder>()  {
+    // Inicializar lista de elemtos a mostrar en Recycler view
     private val listItems: ArrayList<DonationData> = donationsList
 
     @SuppressLint("UseCompatLoadingForDrawables")
     override fun onBindViewHolder(viewHolder: DonationsAdapterViewHolder, position: Int) {
+        //Obtener un elemento de la lista
         val donationItem: DonationData = listItems[position]
 
+        // Definir valores de componentes del layout con los valores dados en la lista
         viewHolder.id.text = donationItem.id
         viewHolder.amount.text = "$ ${donationItem.amount}.00"
         viewHolder.date.text = donationItem.date
     }
 
     class DonationsAdapterViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        // Obtener componentes del layout
         var view: View = view
         var id: TextView = view.findViewById(R.id.donationItemTvIdValue)
         var amount: TextView = view.findViewById(R.id.donationItemTvAmountValue)
--- app/src/main/java/mx/iv/bancodealimentos_project/helper/SliderAdapter.kt
+++ app/src/main/java/mx/iv/bancodealimentos_project/helper/SliderAdapter.kt
@@ -1,7 +1,6 @@
 package mx.iv.bancodealimentos_project.helper
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,7 +12,7 @@ import mx.iv.bancodealimentos_project.R
 import java.util.ArrayList
 
 class SliderAdapter(sliderDataArrayList: ArrayList<AboutUsData>) : SliderViewAdapter<SliderAdapter.SliderAdapterViewHolder>() {
-    private val listItems: List<AboutUsData>
+    private val listItems: List<AboutUsData> = sliderDataArrayList
 
     override fun onCreateViewHolder(parent: ViewGroup): SliderAdapterViewHolder {
         val inflate: View = LayoutInflater.from(parent.context).inflate(R.layout.slider_item, null)
@@ -22,8 +21,10 @@ class SliderAdapter(sliderDataArrayList: ArrayList<AboutUsData>) : SliderViewAda
 
     @SuppressLint("UseCompatLoadingForDrawables")
     override fun onBindViewHolder(viewHolder: SliderAdapterViewHolder, position: Int) {
+        //Obtener un elemento de la lista
         val sliderItem: AboutUsData = listItems[position]
 
+        // Definir valores de componentes del layout con los valores dados en la lista
         viewHolder.title.text = sliderItem.title
         viewHolder.description.text = sliderItem.description
         viewHolder.icon.setImageResource(sliderItem.img)
@@ -34,15 +35,11 @@ class SliderAdapter(sliderDataArrayList: ArrayList<AboutUsData>) : SliderViewAda
         return listItems.size
     }
 
-    class SliderAdapterViewHolder(view: View) : ViewHolder(view) {
-        var view: View = view
+    class SliderAdapterViewHolder(var view: View) : ViewHolder(view) {
+        // Obtener componentes del layout
         var icon: ImageView = view.findViewById(R.id.sliderItemIvIcon)
         var title: TextView = view.findViewById(R.id.sliderItemTvTitle)
         var description: TextView = view.findViewById(R.id.sliderItemTvDescription)
         var layout: LinearLayout = view.findViewById(R.id.sliderLLlayout)
-    }
-
-    init {
-        listItems = sliderDataArrayList
     }
 }
